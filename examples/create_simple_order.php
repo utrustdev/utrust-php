a0f6a7f71e0022ebfa745e12e4d8a655a0c44940
Daniel Coelho <danielmartinscoelho@gmail.com>
2020-01-15 14:32:50 +0000

    Update dotenv package

--- examples/create_simple_order.php
+++ examples/create_simple_order.php
@@ -1,6 +1,6 @@
 <?php
 require_once './vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 use Utrust\ApiClient;
