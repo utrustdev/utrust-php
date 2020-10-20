5d4b96c44ed6782e4143e65b482a55c71b467e13
cswiers <61017681+cswiers@users.noreply.github.com>
2020-10-20 17:12:25 +0200

    Opening brace should be on a new line
    According to lint test opening brace should be on a new line

--- src/Webhook/Event.php
+++ src/Webhook/Event.php
@@ -51,7 +51,8 @@ class Event
      *
      * @return string Order amount.
      */
-    public function getOrderAmount() {
+    public function getOrderAmount() 
+    {
         return $this->payload->resource->amount;
     }
 
